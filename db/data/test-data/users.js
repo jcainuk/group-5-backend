7d6419ff6a6c18c65e7e1bc3561af67b776daac8
Jonathan Cain <jcainuk@gmail.com>
2023-09-20 19:49:59 +0100

    refactor the test data for users

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,7 +1,27 @@
 module.exports = [
-  { username: "test1" },
-  { username: "test2" },
-  { username: "test3" },
-  { username: "test4" },
-  { username: "test5" }
+  {
+    username: "MrTestDrive",
+    avatar_URL: "https://example.com/avatar1.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "MrTestimonial",
+    avatar_URL: "https://example.com/avatar2.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "MrTesty",
+    avatar_URL: "https://example.com/avatar3.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "MrTestRun",
+    avatar_URL: "https://example.com/avatar4.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "MrTestLab",
+    avatar_URL: "https://example.com/avatar5.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  }
 ];
