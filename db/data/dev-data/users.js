1a3009fd155bb0d17330d61caa9ea612819ce5ac
Jonathan Cain <jcainuk@gmail.com>
2023-09-20 22:50:24 +0100

    refactors dev data

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -1,7 +1,27 @@
 module.exports = [
-  { username: "jcainuk" },
-  { username: "Jay7806" },
-  { username: "Lawler45" },
-  { username: "Harrywg" },
-  { username: "A-E-Harding" },
+  {
+    username: "Mr DEV-astation",
+    avatar_URL: "https://example.com/avatar1.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "Ms DEVeloper",
+    avatar_URL: "https://example.com/avatar2.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "Mrs DEVina McCall",
+    avatar_URL: "https://example.com/avatar3.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "Mr Dev Patel",
+    avatar_URL: "https://example.com/avatar4.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "Mr DEVine",
+    avatar_URL: "https://example.com/avatar5.jpg",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  }
 ];
