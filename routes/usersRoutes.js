100b74d3fa2d701a4b9fb2d68d8097dd7cb7b351
Jonathan Cain <jcainuk@gmail.com>
2023-09-20 14:08:16 +0100

    completes post controller for create user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = express.Router();
-const { getUsers } = require("../controllers/usersController");
+const { getUsers, createUser } = require("../controllers/usersController");
 
 // Get all users
 userRouter.get("/", getUsers);
+
+userRouter.post("/", createUser);
 
 module.exports = userRouter;
