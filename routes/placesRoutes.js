66cb604f4fa3ce3094a6cd6f3c076e5419e52958
A-E-Harding <aeg.h@btinternet.com>
2023-09-20 12:17:29 +0100

    creates get places by Id endpoint

--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const placeRouter = express.Router();
-const { getPlaces } = require("../controllers/placesController");
+const { getPlaces, getPlaceById } = require("../controllers/placesController");
 
 // Get all users
 placeRouter.get("/", getPlaces);
+
+placeRouter.get("/:id", getPlaceById)
 
 module.exports = placeRouter;
