b52319368dfdb873d963fd062b08f6c81b8cbdfb
Jonathan Cain <jcainuk@gmail.com>
2023-09-21 17:01:55 +0100

    adds link to real avatar pic url in users dev data

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -1,27 +1,32 @@
 module.exports = [
   {
     username: "MrDEVastation",
-    avatar_URL: "https://example.com/avatar1.jpg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     achievements: { gold: 1, silver: 5, bronze: 10 }
   },
   {
     username: "MsDEVeloper",
-    avatar_URL: "https://example.com/avatar2.jpg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     achievements: { gold: 0, silver: 0, bronze: 0 }
   },
   {
     username: "MrsDEVinaMcCall",
-    avatar_URL: "https://example.com/avatar3.jpg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     achievements: { gold: 3, silver: 0, bronze: 5 }
   },
   {
     username: "MrDevPatel",
-    avatar_URL: "https://example.com/avatar4.jpg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     achievements: { gold: 15, silver: 1, bronze: 0 }
   },
   {
     username: "MrDEVine",
-    avatar_URL: "https://example.com/avatar5.jpg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     achievements: { gold: 2, silver: 11, bronze: 0 }
   }
 ];
