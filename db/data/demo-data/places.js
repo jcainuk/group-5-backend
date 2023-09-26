db071540fe36c21b5601ae2b72ca21bb869c805b
Jonathan Cain <jcainuk@gmail.com>
2023-09-26 11:47:00 +0100

    creates demo data but not complete with guesses yet

---
+++ db/data/demo-data/places.js
@@ -0,0 +1,237 @@
+[
+  {
+    placeName: "Place1", // Liverpool, UK - Near Albert Dock
+    coordinates: [53.4017, -3.0001],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place2", // Liverpool, UK - Near The Beatles Story
+    coordinates: [53.3977, -3.0134],
+    creator: "Jay7806",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place3", // Liverpool, UK - Near Liverpool Cathedral
+    coordinates: [53.3977, -2.9701],
+    creator: "Lawler45",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place4", // Liverpool, UK - Near Tate Liverpool
+    coordinates: [53.4014, -3.0008],
+    creator: "A-E-Harding",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place5", // Liverpool, UK - Near Liverpool ONE
+    coordinates: [53.4044, -2.9906],
+    creator: "Harrywg",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place6", // Liverpool, UK - Near Anfield Stadium
+    coordinates: [53.4308, -2.9609],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place7", // Liverpool, UK - Near Goodison Park
+    coordinates: [53.4391, -2.9649],
+    creator: "Jay7806",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place8", // Liverpool, UK - Near St. George's Hall
+    coordinates: [53.4075, -2.9798],
+    creator: "Lawler45",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place9", // Liverpool, UK - Near World Museum Liverpool
+    coordinates: [53.4079, -2.9804],
+    creator: "A-E-Harding",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place10", // Liverpool, UK - Near Liverpool Empire Theatre
+    coordinates: [53.4058, -2.9781],
+    creator: "Harrywg",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place11", // Liverpool, UK - Near Royal Liver Building
+    coordinates: [53.4052, -3.0143],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+
+  {
+    placeName: "Place12", // Bolton, UK - Near Smithills Hall
+    coordinates: [53.5813, -2.4961],
+    creator: "Harrywg",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place13", // Bolton, UK - Near Hall i' th' Wood
+    coordinates: [53.587, -2.4438],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place14", // Bolton, UK - Near Moses Gate Country Park
+    coordinates: [53.5556, -2.4045],
+    creator: "A-E-Harding",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place15", // Bolton, UK - Near Leverhulme Park
+    coordinates: [53.5752, -2.4272],
+    creator: "Jay7806",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place16", // Bolton, UK - Near Moss Bank Park
+    coordinates: [53.5893, -2.4317],
+    creator: "Lawler45",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place17", // London, UK - Near Buckingham Palace
+    coordinates: [51.5014, -0.1404],
+    creator: "Jay7806",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place18", // London, UK - Near The British Museum
+    coordinates: [51.5194, -0.127],
+    creator: "Lawler45",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place19", // London, UK - Near The Shard
+    coordinates: [51.5045, -0.0865],
+    creator: "A-E-Harding",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place20", // London, UK - Near The London Eye
+    coordinates: [51.5033, -0.1195],
+    creator: "Harrywg",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place21", // London, UK - Near The Tower of London
+    coordinates: [51.5081, -0.0759],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place22", // London, UK - Near The Natural History Museum
+    coordinates: [51.4966, -0.1765],
+    creator: "Jay7806",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place23", // London, UK - Near The Tate Modern
+    coordinates: [51.5076, -0.0995],
+    creator: "Lawler45",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place24", // London, UK - Near Covent Garden
+    coordinates: [51.5134, -0.1244],
+    creator: "A-E-Harding",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place25", // London, UK - Near The Globe Theatre
+    coordinates: [51.5076, -0.0968],
+    creator: "Harrywg",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  },
+  {
+    placeName: "Place26", // London, UK - Near St. Paul's Cathedral
+    coordinates: [51.5138, -0.0984],
+    creator: "jcainuk",
+    imgURL:
+      "https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2593&q=80",
+    guesses: [],
+    votes: 0
+  }
+];
---
+++ db/data/demo-data/users.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    username: "jcainuk",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    achievements: { gold: 1, silver: 5, bronze: 10 }
+  },
+  {
+    username: "Jay7806",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    achievements: { gold: 0, silver: 0, bronze: 0 }
+  },
+  {
+    username: "Lawler45",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    achievements: { gold: 3, silver: 0, bronze: 5 }
+  },
+  {
+    username: "A-E-Harding",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    achievements: { gold: 15, silver: 1, bronze: 0 }
+  },
+  {
+    username: "Harrywg",
+    avatar_URL:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    achievements: { gold: 2, silver: 11, bronze: 0 }
+  }
+];
